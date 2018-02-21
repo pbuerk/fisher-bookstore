3f2191e78a7b2c2c8c3ff34953d947df9638f759
Philip Buerk <philip@Philips-MacBook-Pro.local>
2018-02-21 18:16:59 -0500

    Step 1 Lab 5, adding binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BooksComponent } from './books/books.component';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BooksComponent
+    BooksComponent,
+    BookDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -2,17 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { Book } from '../books/book';
 import { BOOKS } from '../books/mock-book-service';
 
+
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent implements OnInit {
+  
+  selectedBook: Book;
   books = BOOKS;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(book: Book): void {
+    this.selectedBook = book;
   }
 
 }
