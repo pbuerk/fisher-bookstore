f70bdd206e9d332e8ebe973f2ddbcf4ce115a5f2
Philip Buerk <philip@Philips-MacBook-Pro.local>
2018-02-21 18:51:04 -0500

    Added in routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BooksComponent } from './books/books.component';
 import { BookDetailComponent } from './book-detail/book-detail.component';
+import { HomeComponent } from './home/home.component';
+import { OrdersComponent } from './orders/orders.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AppRouterModule } from './router/router.module';
 
 
 
@@ -12,10 +18,15 @@ import { BookDetailComponent } from './book-detail/book-detail.component';
   declarations: [
     AppComponent,
     BooksComponent,
-    BookDetailComponent
+    BookDetailComponent,
+    HomeComponent,
+    OrdersComponent,
+    NavBarComponent,
+    FooterComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule, AppRouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
